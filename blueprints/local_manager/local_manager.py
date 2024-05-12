6bfc1c5bc919c6147d05f02d9eab337e0f1ffa19
AnnieDJ <146434944+AnnieDJ@users.noreply.github.com>
2024-05-12 18:32:46 +1200

    frame
    frame

--- blueprints/local_manager/local_manager.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-local_manager_bp = Blueprint('local_manager', __name__, template_folder='templates')
--- blueprints/staff/staff.py
+++
@@ -1,3 +0,0 @@
-from flask import Blueprint
-
-staff_bp = Blueprint('staff', __name__, template_folder='templates')
