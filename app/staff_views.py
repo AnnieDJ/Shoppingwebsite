c98758d1d8f91d6a05ef789e1f6930ba113d8abb
alin982 <alin982@live.com>
2024-06-11 19:18:21 +1200

    modified change password feature for all roles

--- app/staff_views.py
+++ app/staff_views.py
@@ -66,6 +66,7 @@ def view_profile():
 ## Staff Change Password ##
 @staff_bp.route('/change_password', methods=['POST'])
 @login_required
+@login_required
 def change_password():
     if 'userid' not in session:
         flash("User ID not found in session. Please log in again.", 'danger')
@@ -73,9 +74,12 @@ def change_password():
 
     password = request.form['password']
 
-    if re.search('[a-zA-Z]', password) is None or re.search('[0-9]', password) is None:
-        flash("Password must contain at least one letter and one digit.", 'warning')
-        return redirect(url_for('staff.staff_profile'))
+    # Check for at least one letter, one digit, one uppercase and lowercase letter, and minimum 8 characters
+    pattern = re.compile(r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$')
+
+    if not pattern.match(password):
+        flash("Password must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters.", 'warning')
+        return redirect(url_for('national_manager.national_manager_profile'))
 
     hashed_password = hashing.hash_value(password, salt='ava')
     conn, cursor = db_cursor()
@@ -85,10 +89,13 @@ def change_password():
             'UPDATE user SET password_hash = %s WHERE user_id = %s',
             (hashed_password, session['userid'])
         )
-        conn.commit()
+        conn.commit()  # Ensure the changes are committed to the database
         flash("Your password has been successfully updated.", 'success')
     except MySQLError as e:
         flash(f"An error occurred: {e}", 'danger')
+    finally:
+        cursor.close()
+        conn.close()
 
     return redirect(url_for('staff.staff_profile'))
 
